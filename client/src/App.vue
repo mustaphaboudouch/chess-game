d5eb369b0de1c4a24bff8bac2593385115eb4bc8
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-07-16 13:32:38 +0200

    build: setup client application

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>This is the about page</h1>
+  </div>
+</template>
+
+<style></style>
