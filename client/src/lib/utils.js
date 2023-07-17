a199410fbfd221f297487752a75b109d728936b7
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-07-18 01:13:14 +0200

    feat: add billing button

---
+++ client/src/lib/utils.js
@@ -0,0 +1,7 @@
+function getSubscriptionPlan(user) {
+  const isPro =
+    user.stripePriceId && user.stripeCurrentPeriodEnd?.getTime() + 86_400_000 > Date.now();
+  return isPro ? "PRO" : "FREE";
+}
+
+export { getSubscriptionPlan };
--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,9 +1,19 @@
 <script setup>
 import { storeToRefs } from "pinia";
 import { useAuthStore } from "../stores/auth";
+import axios from "../lib/axios";
 
 const authStore = useAuthStore();
 const { user } = storeToRefs(authStore);
+
+async function handleClick() {
+  try {
+    const response = await axios.get("subscribe");
+    window.location.href = response.data.url;
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
 
 <template>
@@ -14,5 +24,8 @@ const { user } = storeToRefs(authStore);
     <li>Email : {{ user?.email }}</li>
     <li>Role : {{ user?.role }}</li>
     <li>Score : {{ user?.score }}</li>
+    <li>Stripe Customer Id : {{ user?.stripeCustomerId }}</li>
   </ul>
+
+  <button @click="handleClick">Upgrade to PRO</button>
 </template>
